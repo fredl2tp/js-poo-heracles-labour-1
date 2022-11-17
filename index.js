b6707081d7682480158ad7a8be241be9c1b479f0
fredl2tp <fleschira@gmail.com>
2022-11-17 15:50:00 +0100

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require("./src/Fighter");
+
+const Fighterinst = [
+  new Fighter("Heracles🧔", 20, 6),
+  new Fighter("Nemean Lion🦁", 11, 13),
+];
+//console.log(Fighterinst[0].fight(Fighterinst[1].strength))
+//console.log(Fighterinst[0].name, Fighterinst[0].life);
+//console.log(Fighterinst[1].name, Fighterinst[1].life);
+console.log(Fighterinst[0].life - (Math.max(0,(Fighterinst[1].getRandom()-Fighterinst[0].dexterity))));
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,16 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+  //fight(fightAttac) {};
+  getRandom() {
+    return Math.floor(Math.random() * (this.strength)+1);
+  }
+}
+
+module.exports = Fighter;
